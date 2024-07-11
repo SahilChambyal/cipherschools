4adce43b6e07c3a5161da6e24bbd2dcd751b95ff
Sahil Chambyal <chambyal20062003@gmail.com>
2024-07-11 08:49:10 +0530

    Lecture 16

---
+++ lecture 16/first-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name="Sahil"
+  return (
+    <div>
+      <h1 style={{
+        color:'red',
+        textAlign: "center"
+        }}>
+        Good wishes from {name}
+      </h1>
+    </div>
+    
+  );
+}
+
+export default App;
